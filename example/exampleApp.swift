7800fd77361d569b77eefd6ee50a4c48569b391c
Thanawat prathumset <thanawat.pratumsat@gmail.com>
2023-04-24 09:49:08 +0700

    Initial Commit

---
+++ example/exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleApp.swift
+//  example
+//
+//  Created by Thanawat prathumset on 24/4/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
